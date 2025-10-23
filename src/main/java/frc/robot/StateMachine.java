f48880925eec5af83a722045b849bc03e99fccec
tom13131 <bike13@juno.com>
2025-10-23 16:37:41 -0400

    documentation correction

--- src/main/java/frc/robot/StateMachine.java
+++ src/main/java/frc/robot/StateMachine.java
@@ -87,7 +87,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  *   
  *       return stateMachine;
  * }
- * createStateMachine.schedule();
+ * createStateMachine().schedule();
  *</code></pre>
  */
 public class StateMachine extends Command {
